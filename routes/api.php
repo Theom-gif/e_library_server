37a95daae946d9fa770e46e7930ba9c8bea29cf9
Theom-gif <liheang.heng@student.passerellesnumeriques.org.com>
2026-03-09 08:28:04 +0700

    add code backend

--- routes/api.php
+++ routes/api.php
@@ -53,7 +53,6 @@ Route::middleware('auth:sanctum')->group(function () {
     // Example: Posts API
     Route::apiResource('posts', PostController::class);
 });
-
 // Public approved books (read + cover preview)
 Route::get('/books', [BookWorkflowController::class, 'approvedBooks'])->name('api.books.index');
 Route::get('/books/discover', [BookWorkflowController::class, 'discoverBooks'])->name('api.books.discover');
