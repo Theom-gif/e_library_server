5b48b8f6eb08632cb61e768864c1d093312f9157
unknown <thalita.sok@student.passerellesnumeriques.org>
2026-03-10 07:36:08 +0700

    fix upload

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
+    'allowed_origins' => array_filter(array_map('trim', explode(',', env(
+        'CORS_ALLOWED_ORIGINS',
+        'http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173,http://127.0.0.1:5173,http://localhost:4200,http://127.0.0.1:4200'
+    )))),
 
     'allowed_origins_patterns' => [
         '#^https?://192\.168\.\d{1,3}\.\d{1,3}(:\d+)?$#',
